884ed14ee0980ff30453d82e49cf07d990787d3f
Alisamar Husain <zrthxn@gmail.com>
2020-04-03 04:24:48 +0530

    Init

--- basicEngine.cpp
+++ basicEngine.cpp
@@ -4,15 +4,24 @@
 
 using namespace std;
 
+#define SCREEN_WIDTH 120
+#define SCREEN_HEIGHT 40
+
 void draw(int _x, int _y, void * item) {
   CONSOLE_SCREEN_BUFFER_INFO coninfo;
-  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+  HANDLE _hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
   GetConsoleScreenBufferInfo(hConsole, &coninfo);
 
+
+  wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
   // Go to position
-  coninfo.dwCursorPosition.X = _x;
-  coninfo.dwCursorPosition.Y = _y;
-  SetConsoleCursorPosition(hConsole, coninfo.dwCursorPosition);
+  // coninfo.dwCursorPosition.X = _x;
+  // coninfo.dwCursorPosition.Y = _y;
+  // SetConsoleCursorPosition(hConsole, coninfo.dwCursorPosition);
 }
 
 int main() {
